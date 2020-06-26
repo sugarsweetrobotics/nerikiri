c2d4c1e7579badd9bebd029ba7b55866bf8ca1c3
ysuga <ysuga@ysuga.net>
2020-06-26 09:45:35 +0900

    コメントを追加

--- include/nerikiri/base64.h
+++ include/nerikiri/base64.h
@@ -1,3 +1,9 @@
+/***
+ * base64.h
+ * @author Yuki Suga
+ * @copyright SUGAR SWEET ROBOTICS, 2020
+ * @brief base64コーディングのための関数
+ */
 /// Thanks to https://qiita.com/leak4mk0/items/6c7f708dd59d52e0bc5c
 /// LICENSE WTFPL v2
 
@@ -7,22 +13,16 @@
 #ifdef __cplusplus
 extern "C" {
 #endif
-#if 0
-}
-#endif
 
-typedef enum tagBASE64_TYPE {
-    BASE64_TYPE_STANDARD,
-    BASE64_TYPE_MIME,
-    BASE64_TYPE_URL
-} BASE64_TYPE;
+    typedef enum tagBASE64_TYPE {
+        BASE64_TYPE_STANDARD,
+        BASE64_TYPE_MIME,
+        BASE64_TYPE_URL
+    } BASE64_TYPE;
 
-char *base64Encode(const char *data, const size_t size, const BASE64_TYPE type);
-char *base64Decode(const char *base64, size_t *retSize, const BASE64_TYPE type);
+    char *base64Encode(const char *data, const size_t size, const BASE64_TYPE type);
+    char *base64Decode(const char *base64, size_t *retSize, const BASE64_TYPE type);
 
-#if 0
-{
-#endif
 #ifdef __cplusplus
 }
 #endif
