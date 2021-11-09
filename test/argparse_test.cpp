ad09fbfe18395046e6614915c1cf5f8891be7dd8
Sugar Sweet Robotis <ysuga@sugarsweetrobotics.com>
2021-11-09 10:09:28 +0900

    add argparse_test

---
+++ test/argparse_test.cpp
@@ -0,0 +1,20 @@
+
+#include <iostream>
+#include <memory>
+#include <juiz/logger.h>
+#include <juiz/juiz.h>
+#include <juiz/process.h>
+#include <juiz/operation.h>
+//#include "juiz/connection_builder.h"
+
+#include "operations_for_tests.h"
+
+#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
+#include "catch.hpp"
+using namespace juiz;
+
+
+
+
+TEST_CASE( "Connection test", "[ec]" ) {
+}
